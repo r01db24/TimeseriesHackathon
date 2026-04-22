ac80bdb4060cb7981adb41e25c79b24787fda17f
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2026-04-22 16:14:52 +0100

    redesigned and included merged data
    Call any of the data_{} functions to get the dataframes. note the folder is an optional string incase you need to change the folder path and value can be left '' for full dataframe or  rain,temp,pest,yield depending on what specific value you want

--- webapp/dataset_jack.py
+++ webapp/dataset_jack.py
@@ -1,118 +1,240 @@
 import numpy as np
 import pandas as pd
 
+def dfs(folder = '..'):
+    data_rain = to_df(folder+'/rainfall.csv')
+    data_temp = to_df(folder+'/temp.csv')
+    data_pest = to_df(folder + '/pesticides.csv')
+    data_yield = to_df(folder+ '/yield.csv')
+    dataframes = [data_rain, data_temp, data_pest, data_yield]
+    return dataframes
 
 def to_df(csv_path):
+
     df = pd.read_csv(csv_path)
 
-
-    if 'Month' in df:
-        df['Year'] = pd.to_datetime(df[['Year','Month']].assign(day=1)).dt.strftime('%m/%Y')
-        df = df.drop(columns = 'Month')
-
-
+    # 'Shaping Dominiks monthly data'
+    # if 'Month' in df:
+    #     df['Year'] = pd.to_datetime(df[['Year','Month']].assign(day=1)).dt.strftime('%m/%Y')
+    #     df = df.drop(columns = 'Month')
 
 
-    'shaping pesticide data'
+    #'shaping yield data'
+    if 'Element Code' in df: #note 'Item' could be a useful column to have
+        df = df.drop(['Domain Code','Area Code', 'Domain', 'Unit',
+                      'Element Code','Element', 'Item','Item Code','Year Code'], axis = 1)
+        df = df.rename(columns = {'Value':'Yield'})
+
+    #'shaping pesticide data'
     if 'Element' in df:
         df = df.drop(['Domain','Element', 'Item','Unit'], axis = 1)
+        df = df.rename(columns = {'Value':'Pest'})
 
 
-        'shaping yield data'
-        if 'Element Code' in df: #note 'Item' could be a useful column to have
-            df = df.drop(['Domain Code','Area Code',
-                          'Element Code', 'Item Code','Year Code'], axis = 1)
+
 
     df.columns = df.columns.str.replace(' ', '') #gets rid of spaces
 
     'renaming columns'
+    # standard_names = {'year': 'Date',
+    #                   'Year': 'Date',
+    #                   'country': 'Country',
+    #                   'avg_temp' : 'Value',
+    #                   'Area': 'Country',
+    #                   'average_rain_fall_mm_per_year': 'Value',
+    #                   'monthly_rainfall_mm' : 'Value'
+    #                   }
+
     standard_names = {'year': 'Date',
                       'Year': 'Date',
                       'country': 'Country',
-                      'avg_temp' : 'Value',
                       'Area': 'Country',
-                      'average_rain_fall_mm_per_year': 'Value',
-                      'monthly_rainfall_mm' : 'Value'
+                      'average_rain_fall_mm_per_year': 'avg_rain',
                       }
+
 
     df = df.rename(columns = standard_names)
 
     're-arranging into standard order country, year , value'
-    df = df[['Country', 'Date', 'Value']]
+    value_name = df.iloc[:,2].name
+    df = df[['Country', 'Date',value_name]]
 
 
 
 
-    df['Value'] = pd.to_numeric(df['Value'], errors='coerce')# turn spaces to nan
+    df[value_name] = pd.to_numeric(df[value_name], errors='coerce')# turn spaces to nan
 
     return df
 
 
-def data_NaN(csv_path):
-    'origional dataset with NaN values'
-    return  to_df(csv_path)
+
+def align(dataframes):
+    'list of dataframes to align and reseperate'
+
+    df_merged = dataframes[0]
+    for i in range(1, len(dataframes)):
+        df_merged = pd.merge(df_merged, dataframes[i], on=["Date", "Country"], how="outer")
+
+
+    return df_merged
 
 
 
-def data_zeros(csv_path):
+    "df_merged = pd.merge(\n",
+    "    df_temp, \n",
+    "    df_rain, \n",
+    "    on=['year', 'month', 'country'], \n",
+    "    how='inner'\n",
+
+
+
+
+
+
+
+
+
+
+
+
+def data_NaN(folder = '..', value = ''):
+    'origional datasets with NaN values'
+    df_list = dfs(folder = folder)
+    dataframe = align(df_list)
+
+    if value == 'rain':
+        dataframe = dataframe['avg_rain'].values
+
+    elif value == 'temp':
+        dataframe = dataframe['avg_temp'].values
+
+    elif value == 'pest':
+        dataframe = dataframe['Pest'].values
+
+    elif value == 'yield':
+        dataframe = dataframe['Yield'].values
+
+
+    else:
+        pass
+
+    return  dataframe
+
+
+
+def data_zeros(folder = '..', value = ''):
     'replaces empty spaces with 0s'
-    dataframe = to_df(csv_path)
+
+    df_list = dfs(folder = folder)
+    dataframe = align(df_list)
     dataframe = dataframe.fillna(0)
-    return dataframe
+
+    if value == 'rain':
+        dataframe = dataframe['avg_rain'].values
+
+    elif value == 'temp':
+        dataframe = dataframe['avg_temp'].values
+
+    elif value == 'pest':
+        dataframe = dataframe['Pest'].values
+
+    elif value == 'yield':
+        dataframe = dataframe['Yield'].values
+
+
+    else:
+        pass
+
+    return  dataframe
 
 
 
-def data_remove_rows(csv_path):
+def data_remove_rows(folder = '..', value = ''):
     'removes rows with missing data'
-    dataframe = to_df(csv_path)
+    df_list = dfs(folder = folder)
+    dataframe = align(df_list)
     dataframe = dataframe.dropna(axis = 'rows')
+
+    if value == 'rain':
+        dataframe = dataframe['avg_rain'].values
+
+    elif value == 'temp':
+        dataframe = dataframe['avg_temp'].values
+
+    elif value == 'pest':
+        dataframe = dataframe['Pest'].values
+
+    elif value == 'yield':
+        dataframe = dataframe['Yield'].values
+
+    else:
+        pass
+
     return dataframe
 
 
-def data_mean_val(csv_path):
+def data_mean_val(folder = '..', value = ''):
     'replaces NaN with mean values'
-    dataframe = to_df(csv_path)
-    df_removed_nan = data_remove_rows(csv_path)
-    column = pd.isna(dataframe.iloc[:,2]).name
-    means =  df_removed_nan[column].to_numpy()
-    mean = np.mean(means)
-    dataframe[column].fillna(mean,inplace = True)
+    df_list = dfs(folder = folder)
+    dataframe = align(df_list)
+    dataframe = dataframe.fillna(dataframe.mean(numeric_only=True))
+
+    if value == 'rain':
+        dataframe = dataframe['avg_rain'].values
+
+    elif value == 'temp':
+        dataframe = dataframe['avg_temp'].values
+
+    elif value == 'pest':
+        dataframe = dataframe['Pest'].values
+
+    elif value == 'yield':
+        dataframe = dataframe['Yield'].values
+
+    else:
+        pass
+
     return dataframe
 
-def data_median_val(csv_path):
+def data_median_val(folder = '..', value = ''):
     '''replaces NaN with median values (apparently better than mean
      according to a stackoverflow post)'''
 
+    df_list = dfs(folder = folder)
+    dataframe = align(df_list)
+    dataframe = dataframe.fillna(dataframe.median(numeric_only=True))
 
-    dataframe = to_df(csv_path)
-    df_removed_nan = data_remove_rows(csv_path)
-    column = pd.isna(dataframe.iloc[:,2]).name
-    medians =  df_removed_nan[column].to_numpy()
-    median = np.median(medians)
-    dataframe[column].fillna(median,inplace = True)
+    if value == 'rain':
+        dataframe = dataframe['avg_rain'].values
+
+    elif value == 'temp':
+        dataframe = dataframe['avg_temp'].values
+
+    elif value == 'pest':
+        dataframe = dataframe['Pest'].values
+
+    elif value == 'yield':
+        dataframe = dataframe['Yield'].values
+
+    else:
+        pass
+
     return dataframe
 
 
 
 if __name__ == '__main__':
 
-    path = '../rainfall.csv'
 
-    data_rain = to_df(path)
-    data_temp = to_df('../temp.csv')
-    data_pest = to_df('../pesticides.csv')
-    data_yield = to_df('../yield.csv')
+    dataframes_aligned = align(dfs())
 
-    zero = data_zeros(path)
-    removed = data_remove_rows(path)
+    nan_df = data_NaN(value = 'rain')
 
-    mean = data_mean_val(path)
+    zero = data_zeros(value = 'temp')
+    removed = data_remove_rows()
 
-    median = data_median_val(path)
+    mean = data_mean_val(value = 'yield')
 
-
-    domi_path = '../Germany_monthly_1950_2013.csv'
-    domi_data = to_df(domi_path)
-
+    median = data_median_val(value = 'pest')
 
 
